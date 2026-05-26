a7c37ee43f448da77325040b96f650d6bad28d72
GiliFaibish1999 <gilifaibishpro@gmail.com>
2026-05-26 14:41:23 +0200

    workflow fix 26.5.14:41

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 // import { createFileRoute } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
 import portrait from "@/assets/portrait.png";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 // export const Route = createFileRoute("/")({
 //   head: () => ({
@@ -113,6 +114,7 @@ function FlipName() {
 }
 
 function Hero() {
+  const isMobile = useIsMobile();
   return (
     <section
       id="top"
