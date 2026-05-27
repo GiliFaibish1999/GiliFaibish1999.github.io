4ccc68d3d7fd6e3970439771eeb6abe63b19da8d
GiliFaibish1999 <gilifaibishpro@gmail.com>
2026-05-27 15:26:26 +0200

    Simplify portfolio runtime for mobile

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "@tanstack/react-router";
 
-import { getRouter } from "./router";
+import { Index } from "./routes/index";
 import "./styles.css";
-
-const router = getRouter();
-
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
-}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Index />
   </React.StrictMode>,
 );
